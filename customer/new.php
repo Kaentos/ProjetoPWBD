493738c6f8999e2d1a7f3644b7e6d31fd66a1a29
kaentos <miguelmagueijo@hotmail.com>
2021-01-18 16:27:48 +0000

    yes

--- customer/new.php
+++ customer/new.php
@@ -119,7 +119,7 @@
     <link rel="stylesheet" href="/ProjetoPWBD/assets/css/new_inspection.css">
     <script src="/ProjetoPWBD/assets/js/edit_user.js"></script>
     <link rel="icon" href="/ProjetoPWBD/assets/img/icon.png">
-
+    <script src="/ProjetoPWBD/assets/js/messages.js"></script>
     <style>
         .eu-inputBtn {
             justify-content: center;
